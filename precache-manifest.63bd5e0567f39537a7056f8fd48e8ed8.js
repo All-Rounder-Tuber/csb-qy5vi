30335d0b973cab379ef23589ef48cb3c80d5862d
Code Sandbox <hello@codesandbox.io>
2021-08-09 17:52:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.63bd5e0567f39537a7056f8fd48e8ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cee1db41f0b4535acd1b1512079dce8",
+    "url": "https://all-rounder-tuber.github.io/csb-qy5vi/index.html"
+  },
+  {
+    "revision": "e4e6dab65300d6599076",
+    "url": "https://all-rounder-tuber.github.io/csb-qy5vi/static/css/main.9db20b4e.chunk.css"
+  },
+  {
+    "revision": "3f53e773c02da5564ecf",
+    "url": "https://all-rounder-tuber.github.io/csb-qy5vi/static/js/2.ab4f94fe.chunk.js"
+  },
+  {
+    "revision": "e4e6dab65300d6599076",
+    "url": "https://all-rounder-tuber.github.io/csb-qy5vi/static/js/main.cda716ec.chunk.js"
+  },
+  {
+    "revision": "043bfa2d8873f52b5a8e",
+    "url": "https://all-rounder-tuber.github.io/csb-qy5vi/static/js/runtime-main.1e635056.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://all-rounder-tuber.github.io/csb-qy5vi/precache-manifest.63bd5e0567f39537a7056f8fd48e8ed8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://all-rounder-tuber.github.io/csb-qy5vi/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
